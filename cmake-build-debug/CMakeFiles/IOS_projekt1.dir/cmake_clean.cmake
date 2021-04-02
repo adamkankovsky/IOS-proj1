4590e5c06fefa34d0bc103eca5a6f46ffa9f1d0f
adamkankovsky <adamkankovsky@gmail.com>
2021-04-02 21:04:38 +0200

    vsechno

---
+++ cmake-build-debug/CMakeFiles/IOS_projekt1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IOS_projekt1.dir/main.c.o"
+  "IOS_projekt1"
+  "IOS_projekt1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IOS_projekt1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
